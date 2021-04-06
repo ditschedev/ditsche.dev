f38dd9c611876b80563a2c6c4813cad10c56f912
Tobias Dittmann <hello@ditsche.dev>
2021-04-06 09:18:30 +0200

    add og images for basic pages

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -20,6 +20,7 @@ export default function Blog({ posts }) {
     <Container
       title="Blog – Toby Christopher"
       description="Thoughts on the software industry, programming, tech, videography, music, and my personal life."
+      image={`https://ditsche.dev/static/images/blog/og.png`}
     >
       <div className="flex flex-col justify-center items-start max-w-3xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
